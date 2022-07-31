9d589b0328d0bb2689e5648cfd4b30dd0bbe4a65
Andrew Nazarov <xiss@users.noreply.github.com>
2022-07-31 19:07:46 +0300

    Add project files.

---
+++ Task1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string first, string second, int count)
+        {
+            Debug.WriteLine($"first: {first}, second: {second}, number: {count}");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
